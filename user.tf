cbd7d87e986bed6c5eb432de279598c8cc19c96a
willingham <thomas@tshows.us>
2018-12-05 14:22:24 -0600

    cleanup spacing

--- user.tf
+++ user.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_user" "ci" {
 	count = "${var.create_ci_user ? 1 : 0}"
-	name = "${var.distribution_name}-ci"
+	name  = "${var.distribution_name}-ci"
 }
 
 
 resource "aws_iam_user_policy" "ci" {
 	count = "${var.create_ci_user ? 1 : 0}"
-  name = "write_to_origin_and_invalidate_cloudfront"
-  user = "${aws_iam_user.ci.name}"
+  name  = "write_to_origin_and_invalidate_cloudfront"
+  user  = "${aws_iam_user.ci.name}"
 
   policy = <<EOF
 {
