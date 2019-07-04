9d227f6d7493ab030772c4d116785654064e772b
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-04 17:33:38 +0530

    updated to terraform 12

--- main.tf
+++ main.tf
@@ -1,29 +1,43 @@
 data "aws_s3_bucket" "log_bucket" {
-  bucket = "${var.log_bucket}"
+  bucket = var.log_bucket
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin" {
-  comment = "${var.distribution_name}"
+  comment = var.distribution_name
 }
 
 resource "aws_cloudfront_distribution" "distribution" {
-  aliases                 = [
-    "${var.aliases}"
-  ]
-  comment                 = "${var.comment}"
-  custom_error_response   = [
-    "${var.custom_error_responses}"
-  ]
+  aliases = var.aliases
+  comment = var.comment
+  dynamic "custom_error_response" {
+    for_each = [for custom_error_response in  var.custom_error_responses:{
+      error_caching_min_ttl = custom_error_response.error_caching_min_ttl
+      error_code            = custom_error_response.error_code
+      response_code         = custom_error_response.response_code
+      response_page_path    = custom_error_response.response_page_path
+    }]
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
+      error_code            = custom_error_response.value.error_code
+      response_code         = custom_error_response.value.response_code
+      response_page_path    = custom_error_response.value.response_page_path
+    }
+  }
   default_cache_behavior {
     allowed_methods = [
       "HEAD",
-      "GET"
+      "GET",
     ]
     cached_methods = [
       "HEAD",
-      "GET"
+      "GET",
     ]
-    default_ttl             = 3600
+    default_ttl = 3600
     forwarded_values {
       cookies {
         forward = "none"
@@ -32,48 +46,123 @@ resource "aws_cloudfront_distribution" "distribution" {
     }
     lambda_function_association {
       event_type = "origin-request"
-      lambda_arn = "${aws_lambda_function.redirector.qualified_arn}"
+      lambda_arn = aws_lambda_function.redirector.qualified_arn
     }
-    max_ttl                 = 86400
-    min_ttl                 = 0
-    target_origin_id        = "${var.distribution_name}"
-    viewer_protocol_policy  = "redirect-to-https"
+    max_ttl                = 86400
+    min_ttl                = 0
+    target_origin_id       = var.distribution_name
+    viewer_protocol_policy = "redirect-to-https"
   }
-  default_root_object     = "${var.default_root_object}"
-  depends_on              = [
-    "aws_lambda_permission.redirector"
-  ]
-  enabled                 = true
-  is_ipv6_enabled         = true
+  default_root_object = var.default_root_object
+  depends_on          = [aws_lambda_permission.redirector]
+  enabled             = true
+  is_ipv6_enabled     = true
   lifecycle {
-    ignore_changes = [
-      "default_cache_behavior"
-    ]
+    ignore_changes = [default_cache_behavior]
   }
   logging_config {
-    bucket          = "${data.aws_s3_bucket.log_bucket.bucket_domain_name}"
+    bucket          = data.aws_s3_bucket.log_bucket.bucket_domain_name
     include_cookies = false
     prefix          = "cloudfront/${var.distribution_name}/"
   }
-  ordered_cache_behavior  = [
-    "${var.ordered_cache_behaviors}"
-  ]
-  origin {
-    domain_name = "${aws_s3_bucket.origin.bucket_domain_name}"
-    origin_id   = "${var.distribution_name}"
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin.cloudfront_access_identity_path}"
+  dynamic "ordered_cache_behavior" {
+    for_each = [for ordered_cache_behavior in var.ordered_cache_behaviors:{
+      allowed_methods           = ordered_cache_behavior.allowed_methods
+      cached_methods            = ordered_cache_behavior.cached_methods
+      compress                  = ordered_cache_behavior.compress
+      default_ttl               = ordered_cache_behavior.default_ttl
+      field_level_encryption_id = ordered_cache_behavior.field_level_encryption_id
+      max_ttl                   = ordered_cache_behavior.max_ttl
+      min_ttl                   = ordered_cache_behavior.min_ttl
+      path_pattern              = ordered_cache_behavior.path_pattern
+      smooth_streaming          = ordered_cache_behavior.smooth_streaming
+      target_origin_id          = ordered_cache_behavior.target_origin_id
+      trusted_signers           = ordered_cache_behavior.trusted_signers
+      viewer_protocol_policy    = ordered_cache_behavior.viewer_protocol_policy
+
+    }]
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      allowed_methods           = ordered_cache_behavior.value.allowed_methods
+      cached_methods            = ordered_cache_behavior.value.cached_methods
+      compress                  = ordered_cache_behavior.value.compress
+      default_ttl               = ordered_cache_behavior.value.default_ttl
+      field_level_encryption_id = ordered_cache_behavior.value.field_level_encryption_id
+      max_ttl                   = ordered_cache_behavior.value.max_ttl
+      min_ttl                   = ordered_cache_behavior.value.min_ttl
+      path_pattern              = ordered_cache_behavior.value.path_pattern
+      smooth_streaming          = ordered_cache_behavior.value.smooth_streaming
+      target_origin_id          = ordered_cache_behavior.value.target_origin_id
+      trusted_signers           = ordered_cache_behavior.value.trusted_signers
+      viewer_protocol_policy    = ordered_cache_behavior.value.viewer_protocol_policy
+
+      dynamic "forwarded_values" {
+        for_each = lookup(ordered_cache_behavior.value, "forwarded_values", [])
+        content {
+          headers                 = lookup(forwarded_values.value, "headers", null)
+          query_string            = forwarded_values.value.query_string
+          query_string_cache_keys = lookup(forwarded_values.value, "query_string_cache_keys", null)
+
+          dynamic "cookies" {
+            for_each = lookup(forwarded_values.value, "cookies", [])
+            content {
+              forward           = cookies.value.forward
+              whitelisted_names = lookup(cookies.value, "whitelisted_names", null)
+            }
+          }
+        }
+      }
+
+      dynamic "lambda_function_association" {
+        for_each = lookup(ordered_cache_behavior.value, "lambda_function_association", [])
+        content {
+          event_type   = lambda_function_association.value.event_type
+          include_body = lookup(lambda_function_association.value, "include_body", null)
+          lambda_arn   = lambda_function_association.value.lambda_arn
+        }
+      }
     }
   }
-  price_class             = "${var.price_class}"
+  origin {
+    domain_name = aws_s3_bucket.origin.bucket_domain_name
+    origin_id   = var.distribution_name
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin.cloudfront_access_identity_path
+    }
+  }
+  price_class = var.price_class
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
-  tags                    = "${local.tags}"
-  viewer_certificate      = [
-    "${local.viewer_certificates[local.cloudfront_default_certificate ? 0 : 1]}"
-  ]
-  web_acl_id              = "${var.web_acl_id}"
+  tags = local.tags
+  dynamic "viewer_certificate" {
+    for_each = [for viewer_certificate in local.viewer_certificates[local.cloudfront_default_certificate ? 0 : 1]:{
+      acm_certificate_arn            = viewer_certificate.acm_certificate_arn
+      cloudfront_default_certificate = viewer_certificate.cloudfront_default_certificate
+      iam_certificate_id             = viewer_certificate.iam_certificate_id
+      minimum_protocol_version       = viewer_certificate.minimum_protocol_version
+      ssl_support_method             = viewer_certificate.ssl_support_method
+
+    }]
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      acm_certificate_arn            = viewer_certificate.value.acm_certificate_arn
+      cloudfront_default_certificate = viewer_certificate.value.cloudfront_default_certificate
+      iam_certificate_id             = viewer_certificate.value.iam_certificate_id
+      minimum_protocol_version       = viewer_certificate.value.minimum_protocol_version
+      ssl_support_method             = viewer_certificate.value.ssl_support_method
+    }
+  }
+  web_acl_id = var.web_acl_id
 }
+
--- variables.tf
+++ variables.tf
@@ -1,69 +1,70 @@
 variable "acm_certificate_arn" {
   default     = ""
   description = "The ARN of the AWS Certificate Manager certificate that you wish to use with this distribution. The ACM certificate must be in US-EAST-1. If this is left empty, the default certificate will be used"
-  type        = "string"
+  type        = string
 }
 
 variable "aliases" {
   default     = []
   description = "Extra CNAMEs (alternate domain names), if any, for this distribution."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "bucket_name" {
   default     = ""
   description = "The name of the orgin bucket. Defaults to distribution_name"
-  type        = "string"
+  type        = string
 }
 
 variable "comment" {
   default     = ""
   description = "Any comments you want to include about the distribution."
-  type        = "string"
+  type        = string
 }
 
 variable "custom_error_responses" {
   default     = []
   description = "One or more custom error response elements (multiples allowed)."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "default_root_object" {
   default     = "index.html"
   description = "The object that you want CloudFront to return (for example, index.html) when an end user requests the root URL."
-  type        = "string"
+  type        = string
 }
 
 variable "distribution_name" {
   description = "The name of the distribution."
-  type        = "string"
+  type        = string
 }
 
 variable "log_bucket" {
   description = "The log bucket to log CloudFront and S3 logs to."
-  type        = "string"
+  type        = string
 }
 
 variable "ordered_cache_behaviors" {
   default     = []
   description = "An ordered list of cache behaviors resource for this distribution. List from top to bottom in order of precedence. The topmost cache behavior will have precedence 0."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "price_class" {
   default     = "PriceClass_100"
   description = "The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100."
-  type        = "string"
+  type        = string
 }
 
 variable "tags" {
   default     = {}
   description = "A mapping of tags to assign to the resource."
-  type        = "map"
+  type        = map(string)
 }
 
 variable "web_acl_id" {
   default     = ""
   description = "If you're using AWS WAF to filter CloudFront requests, the Id of the AWS WAF web ACL that is associated with the distribution."
-  type        = "string"
+  type        = string
 }
+
