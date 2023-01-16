78422f088ec61b5206b7cb77120e79666d952d94
Joseph Wortmann <joseph.wortmann@gmail.com>
2023-01-16 14:38:52 +0000

    nodejs18.x

--- default-index-redirect.tf
+++ default-index-redirect.tf
@@ -57,7 +57,7 @@ resource "aws_lambda_function" "redirector" {
   }
   publish          = true
   role             = aws_iam_role.redirector.arn
-  runtime          = "nodejs16.x"
+  runtime          = "nodejs18.x"
   source_code_hash = "${path.module}/default-index-redirect/function.zip"
   tags             = local.tags
 }
